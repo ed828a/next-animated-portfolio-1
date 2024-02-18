0d159d6e08fe28036812a4ebdad93bc4d456a85b
Edward Lee <ed828a@gmail.com>
2024-02-18 18:47:42 +1030

    just before portfolio

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,27 @@
 "use client";
 
-import { motion } from "framer-motion";
+import Brain from "@/components/brain";
+import { motion, useInView, useScroll } from "framer-motion";
 import Image from "next/image";
+import { useRef } from "react";
 
 const AboutPage = () => {
+  const containerRef = useRef();
+
+  const { scrollYProgress } = useScroll({ container: containerRef });
+
+  const skillRef = useRef();
+  const isSkillRefInView = useInView(skillRef, {
+    // once: true,
+    margin: "-100px",
+  });
+
+  const experienceRef = useRef();
+  const isExperienceRefInView = useInView(experienceRef, {
+    // once: true,
+    margin: "-100px",
+  });
+
   return (
     <motion.div
       className="h-full"
@@ -12,9 +30,9 @@ const AboutPage = () => {
       transition={{ duration: 2 }}
     >
       {/* container */}
-      <div className="">
+      <div className="h-full overflow-scroll flex" ref={containerRef}>
         {/* text container */}
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 ">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
           <div className="flex flex-col gap-12 justify-center">
             <h1 className="font-bold text-2xl">Biography</h1>
             <p className="text-lg">
@@ -34,11 +52,46 @@ const AboutPage = () => {
               alt="mysign"
               className="ml-auto"
             />
+            {/* BIOGRAPHY SCROLL SVG */}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
           </div>
 
-          <div className="flex flex-col gap-12 justify-center">
-            <h1 className="font-bold text-2xl">Skills</h1>
-            <div className="flex flex-wrap gap-2 md:gap-4">
+          <div className="flex flex-col gap-12 justify-center" ref={skillRef}>
+            <motion.h1
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+              transition={{ delay: 0.2 }}
+              className="font-bold text-2xl"
+            >
+              Skills
+            </motion.h1>
+            <motion.div
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+              transition={{ delay: 0.5 }}
+              className="flex flex-wrap gap-2 md:gap-4"
+            >
               <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 JavaScript
               </div>
@@ -90,96 +143,154 @@ const AboutPage = () => {
               <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 RestAPI
               </div>
-            </div>
+            </motion.div>
+            {/* Skill SCROLL SVG */}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
           </div>
-          <div className="flex flex-col gap-12 justify-center mb-48">
-            <h1 className="font-bold text-2xl">Experiences</h1>
+          <div
+            className="flex flex-col gap-12 justify-center mb-48"
+            ref={experienceRef}
+          >
+            <motion.h1
+              initial={{ x: "-300px" }}
+              animate={isExperienceRefInView ? { x: 0 } : {}}
+              transition={{ delay: 0.2 }}
+              className="font-bold text-2xl"
+            >
+              Experiences
+            </motion.h1>
             {/* experience list */}
-            <div className="">
-              {/* exprience list item */}
-              <div className="flex justify-between h-48 border">
+            <motion.div
+              initial={{ x: "-300px" }}
+              animate={isExperienceRefInView ? { x: 0 } : {}}
+              transition={{ delay: 0.5 }}
+              className="flex flex-col"
+            >
+              {/* exprience list item 1 */}
+              <div className="flex justify-between">
                 {/* left */}
                 <div className="w-1/3">
                   {/* job title */}
-                  <div className="bg-white font-semibold rounded-md">
+                  <div className="bg-white font-semibold rounded-b-lg rounded-s-lg p-3">
                     Senior JavaScript Enginer
                   </div>
                   {/* job description */}
-                  <div className="">
+                  <div className="p-3 text-sm italic">
                     My current employment. Way better than the position before.
                   </div>
                   {/* job date */}
-                  <div className="">2024 - Present</div>
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
                   {/* job company */}
-                  <div className="">Apple</div>
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Apple
+                  </div>
                 </div>
                 {/* center */}
                 <div className="w-1/6">
                   {/* line */}
-                  <div className="">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
                     {/* line circle */}
-                    <div className=""></div>
+                    <div className="w-5 h-5 rounded-full ring-4 ring-red-400 bg-white absolute top-0 -left-2"></div>
                   </div>
                 </div>
                 {/* right */}
                 <div className="w-1/3"></div>
               </div>
-              <div className="">
+              {/* exprience list item 2 */}
+              <div className="flex justify-between">
                 {/* left */}
-                <div className=""></div>
+                <div className="w-1/3 "></div>
                 {/* center */}
-                <div className="">
+                <div className="w-1/6">
                   {/* line */}
-                  <div className="">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
                     {/* line circle */}
-                    <div className=""></div>
+                    <div className="w-5 h-5 rounded-full ring-4 ring-red-400 bg-white absolute top-0 -left-2"></div>
                   </div>
                 </div>
                 {/* right */}
 
-                <div className="">
+                <div className="w-1/3">
                   {/* job title */}
-                  <div className="">Senior JavaScript Enginer</div>
+                  <div className="bg-white font-semibold rounded-b-lg rounded-s-lg p-3">
+                    Senior JavaScript Enginer
+                  </div>
                   {/* job description */}
-                  <div className="">
+                  <div className="p-3 text-sm italic">
                     My current employment. Way better than the position before.
                   </div>
                   {/* job date */}
-                  <div className="">2024 - Present</div>
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
                   {/* job company */}
-                  <div className="">Apple</div>
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Apple
+                  </div>
                 </div>
               </div>
-              <div className="">
+              {/* exprience list item 3 */}
+              <div className="flex justify-between">
                 {/* left */}
-                <div className="">
+                <div className="w-1/3">
                   {/* job title */}
-                  <div className="">Senior JavaScript Enginer</div>
+                  <div className="bg-white font-semibold rounded-b-lg rounded-r-lg p-3">
+                    Senior JavaScript Enginer
+                  </div>
                   {/* job description */}
-                  <div className="">
+                  <div className="p-3 text-sm italic">
                     My current employment. Way better than the position before.
                   </div>
                   {/* job date */}
-                  <div className="">2024 - Present</div>
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
                   {/* job company */}
-                  <div className="">Apple</div>
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Apple
+                  </div>
                 </div>
                 {/* center */}
-                <div className="">
+                <div className="w-1/6">
                   {/* line */}
-                  <div className="">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
                     {/* line circle */}
-                    <div className=""></div>
+                    <div className="w-5 h-5 rounded-full ring-4 ring-red-400 bg-white absolute top-0 -left-2"></div>
                   </div>
                 </div>
                 {/* right */}
-                <div className=""></div>
+                <div className="w-1/3"></div>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
         {/* SVG container */}
-        <div className="hidden"></div>
+        <div className="lg:block w-1/3 xl:w-1/2 sticky top-0 z-30">
+          <Brain scrollYProgress={scrollYProgress} />
+        </div>
       </div>
     </motion.div>
   );
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,16 +1,83 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useRef } from "react";
+
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "React Commerce",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Medium Blog",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Vanilla Book App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Spotify Music App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    link: "https://lama.dev",
+  },
+];
 
 const PortfolioPage = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ target: ref });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
+
   return (
     <motion.div
       className="h-full"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
-      transition={{ duration: 2 }}
+      transition={{ duration: 1 }}
     >
-      <div className="">PortfolioPage</div>
+      <div className="h-[600px] relative" ref={ref}>
+        <div className="w-screen h-[calc(100vh-6rem)] flex justify-center items-center text-8xl text-center">
+          My Works
+        </div>
+        <div className="sticky top-0 h-screen flex gap-4 items-center overflow-hidden">
+          <motion.div style={{ x }} className="flex">
+            {items.map((item) => (
+              <div
+                className={`w-screen h-screen flex items-center justify-center bg-gradient-to-r ${items.color}`}
+                key={item.id}
+              >
+                <div className="flex flex-col gap-8 text-white">
+                  <h1 className="">{item.title}</h1>
+                  <div className="relative">
+                    <Image src={item.img} fill alt="" />
+                  </div>
+                  <p className="">{item.desc}</p>
+                  <Link href={item.link}>
+                    <button className="">See Demo</button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
     </motion.div>
   );
 };
